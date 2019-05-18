b4a22311e0d5b03cae892a4b315ec790f330c226
mgarg1 <mrmohitgarg1990@gmail.com>
2019-05-18 14:21:31 +0530

    solved2

--- python3/koans/about_list_assignments.py
+++ python3/koans/about_list_assignments.py
@@ -9,29 +9,29 @@ from runner.koan import *
 
 class AboutListAssignments(Koan):
     def test_non_parallel_assignment(self):
-        names = ["John", "Smith"]
-        self.assertEqual(__, names)
+        names = ["J", "S"]
+        self.assertEqual(["J", "S"], names)
 
     def test_parallel_assignments(self):
         first_name, last_name = ["John", "Smith"]
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
+        self.assertEqual("John", first_name)
+        self.assertEqual("Smith", last_name)
 
     def test_parallel_assignments_with_extra_values(self):
-        title, *first_names, last_name = ["Sir", "Ricky", "Bobby", "Worthington"]
-        self.assertEqual(__, title)
-        self.assertEqual(__, first_names)
-        self.assertEqual(__, last_name)
+        title, *first_names, last_name = ["S", "R", "B", "W"]
+        self.assertEqual("S", title)
+        self.assertEqual(["R","B"], first_names)
+        self.assertEqual("W", last_name)
 
     def test_parallel_assignments_with_sublists(self):
-        first_name, last_name = [["Willie", "Rae"], "Johnson"]
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
+        first_name, last_name = [["W", "R"], "J"]
+        self.assertEqual(["W", "R"], first_name)
+        self.assertEqual("J", last_name)
 
     def test_swapping_with_parallel_assignment(self):
-        first_name = "Roy"
-        last_name = "Rob"
+        first_name = "R"
+        last_name = "S"
         first_name, last_name = last_name, first_name
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
+        self.assertEqual("S", first_name)
+        self.assertEqual("R", last_name)
 
--- python3/koans/about_string_manipulation.py
+++ python3/koans/about_string_manipulation.py
@@ -8,14 +8,14 @@ class AboutStringManipulation(Koan):
     def test_use_format_to_interpolate_variables(self):
         value1 = 'one'
         value2 = 2
-        string = "The values are {0} and {1}".format(value1, value2)
-        self.assertEqual(__, string)
+        string = "are {0} and {1}".format(value1, value2)
+        self.assertEqual("are one and 2", string)
 
     def test_formatted_values_can_be_shown_in_any_order_or_be_repeated(self):
-        value1 = 'doh'
-        value2 = 'DOH'
+        value1 = 'd'
+        value2 = 'D'
         string = "The values are {1}, {0}, {0} and {1}!".format(value1, value2)
-        self.assertEqual(__, string)
+        self.assertEqual("The values are D, d, d and D!", string)
 
     def test_any_python_expression_may_be_interpolated(self):
         import math # import a standard python module with math functions
@@ -23,24 +23,24 @@ class AboutStringManipulation(Koan):
         decimal_places = 4
         string = "The square root of 5 is {0:.{1}f}".format(math.sqrt(5),
             decimal_places)
-        self.assertEqual(__, string)
+        self.assertEqual("The square root of 5 is 2.2361", string)
 
     def test_you_can_get_a_substring_from_a_string(self):
         string = "Bacon, lettuce and tomato"
-        self.assertEqual(__, string[7:10])
+        self.assertEqual('let', string[7:10])
 
     def test_you_can_get_a_single_character_from_a_string(self):
         string = "Bacon, lettuce and tomato"
-        self.assertEqual(__, string[1])
+        self.assertEqual('a', string[1])
 
     def test_single_characters_can_be_represented_by_integers(self):
-        self.assertEqual(__, ord('a'))
-        self.assertEqual(__, ord('b') == (ord('a') + 1))
+        self.assertEqual(97, ord('a'))
+        self.assertEqual(True, ord('b') == (ord('a') + 1))
 
     def test_strings_can_be_split(self):
         string = "Sausage Egg Cheese"
         words = string.split()
-        self.assertListEqual([__, __, __], words)
+        self.assertListEqual(["Sausage","Egg","Cheese"], words)
 
     def test_strings_can_be_split_with_different_patterns(self):
         import re #import python regular expression library
@@ -50,7 +50,7 @@ class AboutStringManipulation(Koan):
 
         words = pattern.split(string)
 
-        self.assertListEqual([__, __, __, __], words)
+        self.assertListEqual(["the","rain",";in,spain"_, __, __, __], words)
 
         # Pattern is a Python regular expression pattern which matches ',' or ';'
 
